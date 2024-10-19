b94dc6f0e8b3aae603cfceb067e8ebac795132a4
Stephen Ingham <smi505@york.ac.uk>
2024-10-19 21:16:44 +0100

    chore: removed unnecessary saveToStorage call for step value in handleStepForward method and moved updatePRDetails method check to run when next step is to be 4 only

--- src/utilities/setUpUtilities.ts
+++ src/utilities/setUpUtilities.ts
@@ -59,15 +59,17 @@ const handleStepForward = (props: HandleStepForwardProps) => {
 
   if (newStep === 3) {
     saveToStorage("username", repoOwner);
-    saveToStorage("step", newStep);
+    saveToStorage("patCode", initialValuePAT);
   }
 
-  if (activeNumPRs.length !== 0) {
-    console.log("activeNumPRs array is not empty");
-    updatePRDetails({ setActiveNumPRs, activeNumPRs, repoOwner });
-    saveToStorage("activeNumPRs", activeNumPRs);
-  } else {
-    console.log("activeNumPRs array is empty");
+  if (newStep === 4) {
+    if (activeNumPRs.length !== 0) {
+      console.log("activeNumPRs array is not empty");
+      updatePRDetails({ setActiveNumPRs, activeNumPRs, repoOwner });
+      saveToStorage("activeNumPRs", activeNumPRs);
+    } else {
+      console.log("activeNumPRs array is empty");
+    }
   }
 
   // set step to new incremented value
