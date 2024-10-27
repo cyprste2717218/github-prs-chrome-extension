510961bd6ab2fbcb887ee93c90ad9379b701e858
Stephen Ingham <smi505@york.ac.uk>
2024-10-27 20:39:35 +0000

    chore: altered puppeteer e2e tests to wait until page load before running new unit test validating presence of h1 element text

--- test/index.test.js
+++ test/index.test.js
@@ -10,20 +10,34 @@ let puppeteerArgs = [
 	`--load-extension=${EXTENSION_PATH}`
 ];
 
-beforeEach(async () => {
-	browser = await puppeteer.launch({
-	  headless: false,
-	  args: puppeteerArgs,
-	});
-  });
-  
-  afterEach(async () => {
-	await browser.close();
-	browser = undefined;
-  });
+describe('Extension Tests', () => {
 
+	beforeEach(async () => {
+		browser = await puppeteer.launch({
+		  headless: false,
+		  args: puppeteerArgs,
+		});
+	  });
+	  
+	afterEach(async () => {
+		await browser.close();
+		browser = undefined;
+	  });
+	
+	
+	it('extension renders correctly', async () => {
+		
+		const page = await browser.newPage();
+		const extensionUrl = `chrome-extension://${EXTENSION_ID}/index.html`;
+		await page.goto(extensionUrl, {waitUntil: ['domcontentloaded', "networkidle2"]});
 
-  test('popup renders correctly', async () => {
-	const page = await browser.newPage();
-	await page.goto(`chrome-extension://${EXTENSION_ID}/index.html`);
-  });
+        const popupHeading = await page.$eval('h1', (e => e.innerText));
+
+		
+        expect(popupHeading).toEqual('Welcome to Github PR Tracker!');
+		
+
+	  });
+
+})
+
