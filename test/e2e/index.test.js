56a7346f5ab94c1c83e6f59f9a25c2af3880bd77
Stephen Ingham <smi505@york.ac.uk>
2024-10-27 21:04:45 +0000

    chore: added test:e2e command to test/package.json

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 
-const EXTENSION_PATH = path.join(process.cwd(), '../dist');
+const EXTENSION_PATH = path.join(process.cwd(), '../../dist');
 const EXTENSION_ID = 'ifihfononohiajgdcakilbjemapgbkpe';
 
 let browser;
@@ -10,34 +10,29 @@ let puppeteerArgs = [
 	`--load-extension=${EXTENSION_PATH}`
 ];
 
-describe('Extension Tests', () => {
+beforeEach(async () => {
+	browser = await puppeteer.launch({
+	  headless: false,
+	  args: puppeteerArgs,
+	});
+  });
+  
+afterEach(async () => {
+	await browser.close();
+	browser = undefined;
+  });
 
-	beforeEach(async () => {
-		browser = await puppeteer.launch({
-		  headless: false,
-		  args: puppeteerArgs,
-		});
-	  });
-	  
-	afterEach(async () => {
-		await browser.close();
-		browser = undefined;
-	  });
+it('User can enter github/org username, choose repos to track and view the tracked repo cards',async () => {
+		
+	const page = await browser.newPage();
+	const extensionUrl = `chrome-extension://${EXTENSION_ID}/index.html`;
+	await page.goto(extensionUrl, {waitUntil: ['domcontentloaded', "networkidle2"]});
+
+	const Heading = await page.$eval('h1', (e => e.innerText));
+
 	
-	
-	it('extension renders correctly', async () => {
-		
-		const page = await browser.newPage();
-		const extensionUrl = `chrome-extension://${EXTENSION_ID}/index.html`;
-		await page.goto(extensionUrl, {waitUntil: ['domcontentloaded', "networkidle2"]});
+	expect(Heading).toEqual('Welcome to Github PR Tracker!');
 
-        const popupHeading = await page.$eval('h1', (e => e.innerText));
-
-		
-        expect(popupHeading).toEqual('Welcome to Github PR Tracker!');
-		
-
-	  });
 
 })
 
