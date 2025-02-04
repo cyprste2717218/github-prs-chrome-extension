1489e9a9cb7f4e5de004388ad5616aa9c7fe88fb
Stephen Ingham <stephenmichael833@gmail.com>
2025-02-04 23:08:46 +0000

    Merging e2e testing setup changes from dev into main (#21)
    * chore: set up initial puppeteer testing config
    
    * chore: set test runner to build extension before running jest puppeteer tests
    
    * chore: moved readme images to new dir 'images' to decrease build time of extension
    
    * chore: moved args for puppeeteer testing config into array, puppeteerArgs
    
    * chore: altered puppeteer e2e tests to wait until page load before running new unit test validating presence of h1 element text
    
    * chore: reorganised test structure into integration. e2e and unit directories
    
    * chore: added test:e2e command to test/package.json
    
    * chore: added extra step to e2e index test and refactored test/package.json commands to run seperate command to create pre-build
    
    * chore: further development of e2e test for user-flow with username option
    
    * chore: further development of e2e test for user-flow with username option
    
    * chore: hyperlinked 'load an unpakced build' text to local setup section in README
    
    * chore: brought in jest to simplify running puppeteer tests and fixed bug with wrong dir being used to run puppeteer tests with
    
    * chore: removed jest from top level package and package-lock as added accidentally
    
    * fix: replaced faulty waitForTimeout method call with .waitForNetworkIdle method call in e2e test
    
    * chore: added repo checkbox selection to index e2e test
    
    * chore: finished functionality of first e2e test in index.test.js
    
    * chore: formatting change in index.test.js
    
    * chore: created an .index.unit.test.js and moved puppeteer and jest deps to be installed on top level dir and migrated jest command aliases for e2e and unit tests to top level package.json
    
    * chore: removed unneeded .gitkeep files from test dir
    
    * chore: added command alias for integration tests
    
    * chore: altered test command to not run unit and integration tests while not implemented
    
    * chore: added .nvmrc file setting to 18.17.0
    
    * chore: created test.yml workflow
    
    * chore: added npm audit step to test.yml
    
    * fix: corrected run of prettier command by removing unnecessary run prefix for run-pretty command
    
    * fix: broke up run prettier and build/test commands into separate workflow steps
    
    * Update test.yml
    
    * fix: corrected test command to run e2e tests by using updated end alias instead of e2e for jest tests
    
    * chore: added additional args to puppeteer to aid runnong of e2e tests in ci
    
    * chore: changed headless to true for e2e index test
    
    * chore: altered puppeteer config for e2e test
    
    * chore: removed test step to fix issues later, and added prettier format check step
    
    * fix: ran prettier check to correct formatting issues in index.end.test.js

--- src/components/RepoCardComponents.tsx
+++ src/components/RepoCardComponents.tsx
@@ -56,6 +56,7 @@ const DisplayCardComponent = ({
       <Card
         className="w-[200px] h-[200px] shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out"
         style={{ margin: "5px" }}
+        id={`displayCard-${cardName}`}
       >
         <CardHeader style={{ height: "100px" }}>
           <CardTitle>{cardName}</CardTitle>
@@ -197,7 +198,10 @@ const PreviewCardComponent = ({
   }
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", padding: "10px" }}>
+    <div
+      id={`previewCard-${name}`}
+      style={{ display: "flex", justifyContent: "center", padding: "10px" }}
+    >
       <Collapsible open={cardExpanded} onOpenChange={setCardExpanded}>
         <Card className="w-[400px]">
           <CardHeader>
---
+++ test/e2e/index.end.test.js
@@ -0,0 +1,84 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+const EXTENSION_PATH = path.resolve(process.cwd(), "dist");
+const EXTENSION_ID = "ifihfononohiajgdcakilbjemapgbkpe";
+
+let browser;
+let puppeteerArgs = [
+  `--disable-extensions-except=${EXTENSION_PATH}`,
+  `--load-extension=${EXTENSION_PATH}`,
+];
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: "new",
+    args: puppeteerArgs,
+  });
+});
+
+afterEach(async () => {
+  await browser.close();
+  browser = undefined;
+});
+
+it("User can enter github/org username, choose repos to track and view the tracked repo cards", async () => {
+  const page = await browser.newPage();
+  const extensionUrl = `chrome-extension://${EXTENSION_ID}/index.html`;
+  await page.goto(extensionUrl, {
+    waitUntil: ["domcontentloaded", "networkidle2"],
+  });
+
+  const Heading = await page.$eval("h1", (e) => e.innerText);
+  expect(Heading).toEqual("Welcome to Github PR Tracker!");
+
+  // check username entry button is present in display for user to click
+  const UsernameEntryButton = await page.$eval(
+    "#username-entry-button",
+    (e) => e.innerText
+  );
+  expect(UsernameEntryButton).toEqual("Enter Github Username/Org Name");
+
+  // checking extension changes to step 2 UI after pressing the Username entry button
+  await page.click("#username-entry-button");
+
+  const UsernameTextEntryButton = await page.$("#username");
+  const SubmitUsernameButton = await page.$("#submit-button");
+
+  expect(UsernameTextEntryButton).toBeTruthy();
+  expect(UsernameTextEntryButton).not.toBeNull();
+  expect(SubmitUsernameButton).toBeTruthy();
+  expect(SubmitUsernameButton).not.toBeNull();
+
+  // user enters Github username/org name and submits
+  await page.type("#username", "facebook");
+  await page.click("#submit-button");
+
+  // user selects repos from list of displayed preview repos for tracking
+
+  const SubTitleHeading = await page.$eval("h2", (e) => e.innerText);
+  expect(SubTitleHeading).toEqual(
+    "Enter the Github Username/Org Name to Track"
+  );
+
+  await page.waitForNetworkIdle();
+  const previewCards = await page.$$('[id^="previewCard-"]');
+  expect(previewCards.length).toBeGreaterThanOrEqual(5);
+
+  // checking four initial repositories for tracking, will need to make this more robust
+
+  await page.click("#checkakd");
+  await page.click("#checkAx");
+  await page.click("#checkbetween-meals");
+  await page.click("#checkbookworm");
+
+  // click next button
+  await page.click("#next-button");
+
+  // checking extension changes to step 3 UI after pressing the next button
+  const SubTitleHeading2 = await page.$eval("h2", (e) => e.innerText);
+  expect(SubTitleHeading2).toEqual("Your Repositories");
+
+  const displayCards = await page.$$('[id^="displayCard-"]');
+  expect(displayCards.length).toBeGreaterThanOrEqual(4);
+});
