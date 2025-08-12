87f29a566a3589aa86074f892689e0c5ecf5592d
Stephen Ingham <smi505@york.ac.uk>
2025-08-12 19:16:03 +0100

    chore: added ui for setting polling rate via slider

--- src/components/SettingsComponent.tsx
+++ src/components/SettingsComponent.tsx
@@ -9,33 +9,39 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Slider } from "./ui/slider";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const SettingsComponent = ({}) => {
   return (
     <div className="flex w-full max-w-sm flex-col gap-6">
       <Tabs defaultValue="configuration">
-        <TabsList>
-          <TabsTrigger value="configuration">Configuration</TabsTrigger>
-          <TabsTrigger value="accessibility">Accessibility</TabsTrigger>
+        <TabsList className="flex w-full">
+          <TabsTrigger className="w-full" value="configuration">
+            Configuration
+          </TabsTrigger>
+          <TabsTrigger className="w-full" value="accessibility">
+            Accessibility
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="configuration">
           <Card>
-            <CardHeader>
-              <CardTitle>Configuration</CardTitle>
-              <CardDescription>
-                Make changes to your account here. Click save when you&apos;re
-                done.
-              </CardDescription>
-            </CardHeader>
             <CardContent className="grid gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-name">Name</Label>
-                <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-username">Username</Label>
-                <Input id="tabs-demo-username" defaultValue="@peduarte" />
+              <div className="grid gap-3 text-left">
+                <Label className="mt-7" htmlFor="request-rate-slider">
+                  Github API Request Rate (per min)
+                </Label>
+                <CardDescription>
+                  Note: Applies only to requests made with a Personal Access
+                  Token
+                </CardDescription>
+                <Slider
+                  defaultValue={[50]}
+                  max={100}
+                  step={1}
+                  className={""}
+                  id="request-rate-slider"
+                />
               </div>
             </CardContent>
             <CardFooter>
