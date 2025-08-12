242eee9b19f69eac6bd23d846b4115a067f12590
Stephen Ingham <smi505@york.ac.uk>
2025-08-12 19:17:38 +0100

    chore: removed placeholder content in configuration settings page configuration tab

--- src/components/SettingsComponent.tsx
+++ src/components/SettingsComponent.tsx
@@ -50,28 +50,7 @@ const SettingsComponent = ({}) => {
           </Card>
         </TabsContent>
         <TabsContent value="accessibility">
-          <Card>
-            <CardHeader>
-              <CardTitle>Accessibility</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you&apos;ll be logged
-                out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-current">Current password</Label>
-                <Input id="tabs-demo-current" type="password" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-new">New password</Label>
-                <Input id="tabs-demo-new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
+          <Card></Card>
         </TabsContent>
       </Tabs>
     </div>
