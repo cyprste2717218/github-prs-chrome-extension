faa11bd02ccf55051b36769947c7bd5be53685f0
Stephen Ingham <smi505@york.ac.uk>
2025-11-05 22:30:04 +0000

    chore: updated import for React Fragment component

--- src/components/repo-cards/AllTrackedRepoCards.tsx
+++ src/components/repo-cards/AllTrackedRepoCards.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react/jsx-runtime";
+import { Fragment } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import {
   AllTrackedRepoCardProps,
