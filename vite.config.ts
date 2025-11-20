a193f067275b1379f4c502f028c7051983ce8b99
Stephen Ingham <smi505@york.ac.uk>
2025-11-20 14:38:16 +0000

    chore: making pre-requisite changes for tailwind v3 to v4 migration using tailwind npx utility to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   build: {
@@ -37,7 +38,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
